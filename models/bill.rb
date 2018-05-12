40ef5c9111fb624203fa853a8e9c6c05bbd70d04
tgauguet <tgauguet@student.42.fr>
2018-05-12 12:19:37 +0200

    update: add level 4

--- models/bill.rb
+++ models/bill.rb
@@ -6,13 +6,15 @@ class Bill
     @index = query.index
     @level = query.level
     @consumption = level == 1 ? query.yearly_consumption : query.user.yearly_consumption
-    @price = base_price.to_f
-    @price = price_with_discount unless level == 1
     @user_id = query.user ? query.user.id : query.id
   end
 
   def generate_bill
-    if level == 3
+    price = base_price.to_f
+    price = price_with_discount(price) unless level == 1
+    price = green_contract_price(price) if query.green
+
+    if (3..6) === level
       commission =  Commission.new(price: price, year_sum: query.contract_length)
                               .generate_commission
       { commission: commission, id: index, price: price.to_format, user_id: user_id }
@@ -27,18 +29,18 @@ class Bill
     query.provider.price_per_kwh * consumption
   end
 
-  def price_with_discount
+  def price_with_discount(price)
     discount = case query.contract_length.to_i
       when 1 then 10
       when 2,3 then 20
       when 3..Float::INFINITY then 25
     end
 
-    if discount
-      tmp = price
-      tmp2 = price
-      price = (tmp2 - (tmp * discount / 100)).to_f
-    end
+    price = (price - (price * discount / 100)).to_f if discount
+  end
+
+  def green_contract_price(price)
+    price - consumption * 0.05
   end
 
 end
